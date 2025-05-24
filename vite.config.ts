1247cf71dddf69d6fa7789453221c35a09edc62a
Anas Rauf <anas.hostengine@gmail.com>
2025-05-24 21:28:24 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,12 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    allowedHosts: "all",
   },
   preview: {
     host: "0.0.0.0",
     port: 5173,
+    allowedHosts: "all",
   },
   plugins: [
     react(),
